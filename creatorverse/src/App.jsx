c39dcbc42c014b589e751296056485d0c87a05bb
Joaquin Villegas <villegasjoaco11@gmail.com>
2025-12-29 11:00:17 +0100

    feat: Enhance UI with gradient backgrounds and animations

--- creatorverse/src/App.jsx
+++ creatorverse/src/App.jsx
@@ -11,20 +11,40 @@ import Footer from './components/Footer.jsx';
 function App() {
 
   return (
-   
-    <div className="app-wrapper">
-      <NavBar />
-      <Toaster position="top-center" toastOptions={{
-        duration: 3000
-      }} />
-      <Routes>
-        <Route path="/" element={<ShowCreators />} />
-        <Route path="/add" element={<AddCreator />} />
-        <Route path="/edit/:id" element={<EditCreator />} />
-        <Route path="/view/:id" element={<ViewCreator />} />
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      <div className="gradients-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+              <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive"></div>
+        </div>
+      </div>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <NavBar />
+        <Toaster position="top-center" toastOptions={{
+          duration: 3000
+        }} />
+        <Routes>
+          <Route path="/" element={<ShowCreators />} />
+          <Route path="/add" element={<AddCreator />} />
+          <Route path="/edit/:id" element={<EditCreator />} />
+          <Route path="/view/:id" element={<ViewCreator />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ creatorverse/src/components/InteractiveBubble.jsx
@@ -0,0 +1,14 @@
+import '../css/index.css';
+
+const InteractiveBubble = () => {
+  return (
+    <div 
+      className="interactive"
+      style={{
+        pointerEvents: 'none',
+      }}
+    />
+  );
+};
+
+export default InteractiveBubble;
